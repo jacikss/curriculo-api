ed40810b73bf3cdf39ddcd0beb41b525f68ec963
jacikss <jacikss@gmail.com>
2025-04-30 20:06:40 -0300

    Removendo subpasta duplicada curriculo-api

--- curriculo-api/src/main/java/com/example/curriculo_api/repository/PessoaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.curriculo_api.repository;
-
-import com.example.curriculo_api.model.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
-}
